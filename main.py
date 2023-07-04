06965ed35f4332f99b3913e321ff0debac5114bc
kobay9iq <kby9iq@gmail.com>
2023-07-04 04:08:12 +0300

    reset, settings, curret promt, default

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from revChatGPT.V3 import Chatbot
-from random import randint
 import telebot
+from telebot import types
+import config
 import datetime
 
 
 bot = telebot.TeleBot("6206728258:AAGBhCLfGmDfwTy-pD5P4_SyRIRkY6_Cfss")
-chatbot = Chatbot(api_key="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")
+chatbot = Chatbot(
+    api_key="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")
 
-firstMemePromt = "Ответь на сообщение {} в стиле оксимирона, без своих комментариев (это очень важно), сообщение на русском, уточни, что ты оксимирон, и скажи дату"
-secondMemePromt = "Ответь на сообщение {} строчкой из песни оксимирона, не забудь уточнить, что это сказал оксимирон, и название песни"
+defaultManualMemePromt = "Ответь на сообщение {} в стиле оксимирона, без своих\
+комментариев (это очень важно), сообщение на русском, уточни,\
+что ты оксимирон, и скажи дату"
+defaultRandomMemePromt = "Ответь на сообщение {} строчкой из песни оксимирона,\
+не забудь уточнить, что это сказал оксимирон, и название песни"
 
 last_message_time = None
 
+manualMemePromt = defaultManualMemePromt
+randomMemePromt = defaultRandomMemePromt
 
-def MemePromt(message):
-    if randint(0,1) == 0:
-        answer = chatbot.ask(firstMemePromt.format(message))
+
+def MemePromt(message, manual):
+    if manual:
+        answer = chatbot.ask(manualMemePromt.format(message))
         return answer
     else:
-        answer = chatbot.ask(secondMemePromt.format(message))
+        answer = chatbot.ask(randomMemePromt.format(message))
         return answer
 
 
@@ -29,55 +37,62 @@ def Promt(message):
         bot.reply_to(message, chatbot.ask(message_text))
     else:
         bot.reply_to(message, chatbot.ask("Кто ты?"))
-        
+
 
 @bot.message_handler(commands=['reset'])
 def Reset(message):
     chatbot.reset()
+    bot.reply_to(message, "Успешный сброс!")
 
 
-# @bot.message_handler(commands=['settings'])
-# def NumOfPromt(message):
-#     if message.chat.type == 'private' and message.chat.username == "kobay9iq":
-#         bot.send_message(message.chat.id, 
-#                         f'''Текущий первый промт:\n
-#                         {firstMemePromt}. \n
-#                         Текущий второй промт: \n
-#                         {secondMemePromt}.'''
-#                         )
-#         bot.send_message(message.chat.id, "Какой промт хотите поменять?")
-#         bot.register_next_step_handler(message, ChangingPromt)
+@bot.message_handler(commands=['settings'])
+def NumOfPromt(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton("Рандом промт")
+    button2 = types.KeyboardButton("Промт ручного вызова")
+    markup.add(button1, button2)
 
-# def ChangingPromt(message):
-#     bot.send_message(message.chat.id, '''Какой будет новый промт?\n 
-#                     Не забудь добавить {} на месте сообщения юзера''')
-#     if message.text == "1":
-#         bot.register_next_step_handler(message, ChangingFirtstPromt)
-#     elif message.text == "2":
-#         bot.register_next_step_handler(message, ChangingSecondPromt)
-#     else:
-#         bot.send_message(message.chat.id, "Неверное число (или это не число?)")
-#         return
+    bot.send_message(message.chat.id, f'*Текущий рандом-промт:*\n\
+{randomMemePromt}.\n*Текущий промт ручного вызова:*\n\
+{manualMemePromt}.', parse_mode="Markdown", reply_markup=markup)
 
-# def ChangingFirtstPromt(message):
-#     firstMemePromt = message.text
-#     bot.send_message(message.chat.id, "Успешно!")
+
+@bot.callback_query_handler(func=lambda call: True)
+def SettingButtonPressed(call):
+    print("1")
+    if call.data == "Рандом промт":
+        print("2")
+        bot.send_message(call.message.chat.id, "1")
     
-# def ChangingSecondPromt(message):
-#     secondMemePromt = message.text
-#     bot.send_message(message.chat.id, "Успешно!")
 
+@bot.message_handler(commands=['currentPromts'])
+def CurrentPromts(message):
+    bot.send_message(message.chat.id, f"*Рандом-пронт:* {manualMemePromt}\n\
+*Промт ручного вызова:* {randomMemePromt}", parse_mode="Markdown")
+
+
+@bot.message_handler(commands=["default"])
+def BackPromtsToDefault(message):
+    global manualMemePromt
+    global randomMemePromt
+    manualMemePromt = defaultManualMemePromt
+    randomMemePromt = defaultRandomMemePromt
+    
+    bot.reply_to(message, "Промты возращены к значениям по умолчанию")
+    CurrentPromts(message)
 
 @bot.message_handler(content_types=['text'])
 def MemeReply(message):
     global last_message_time
-    if len(message.text) >= 5 and not(message.text.startswith('/')):
-        if (last_message_time is None or datetime.datetime.now() > last_message_time + datetime.timedelta(seconds=300)):
-            bot.reply_to(message, MemePromt(message.text))
-            last_message_time = datetime.datetime.now()
+    if len(message.text) >= 5 and not (message.text.startswith('/')):
+        if (last_message_time is None 
+            or (datetime.datetime.now() >
+            last_message_time + datetime.timedelta(seconds=300))):
+                bot.reply_to(message, MemePromt(message.text, manual=False))
+                last_message_time = datetime.datetime.now()
         elif message.text.startswith('$'):
-            bot.reply_to(message, MemePromt(message.text))
-            
+            bot.reply_to(message, MemePromt(message.text, manual=True))
 
-print("STARTED")            
+
+print("STARTED")
 bot.infinity_polling()
